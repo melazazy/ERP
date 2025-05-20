d683b83e3cc208fec355db661f83eda179e0e3e2
melazazy <melazazy52@gmail.com>
2025-05-21 01:52:45 +0300

    subcat2

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     use HasFactory;
-
+    
     // protected $table = 'subcategories';
 
     protected $fillable = ['name', 'category_id'];
