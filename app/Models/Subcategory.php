84546061746b6caecf041fcf5771e60c53b19e30
melazazy <melazazy52@gmail.com>
2025-05-21 01:50:14 +0300

    subcat

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -9,7 +9,7 @@ class Subcategory extends Model
 {
     use HasFactory;
 
-    protected $table = 'subcategories';
+    // protected $table = 'subcategories';
 
     protected $fillable = ['name', 'category_id'];
 
